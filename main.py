369728161c3ab99b195eddae5953f4b6c16589ad
meship <101132768+meship@users.noreply.github.com>
2023-12-23 18:02:11 +0200

    Add the matrix method and chenge the matlap project to work more conveniently.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import json
 
 import numpy as np
-import scipy.io
+import re
 import cv2
-from geometry import calculate_H, to_non_homogenous
+from geometry import calculate_H, to_non_homogenous, calculate_height_using_camera_matrix
 from choose_coor import choose_cord
 from ClusteringWithHoughLines import find_vanishing_points
 from utilities import draw_line_on_img
@@ -28,6 +28,19 @@ def choose_coordinates(image, message):
     points.sort(key=lambda point: point[1], reverse=True)
     return points
 
+def extract_points_from_text(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            text = file.readlines()
+            h1 = tuple([int(float(num)) for num in text[1][:-2].strip("(").split(",")])
+            h2 = tuple([int(float(num)) for num in text[2][:-2].strip("(").split(",")])
+            vertical = tuple([int(float(num)) for num in text[4][:-2].strip("(").split(",")])
+        return  h1, h2, vertical
+
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+
 
 if __name__ == "__main__":
     # good images: 1080104, P1080106, 1080119
@@ -36,25 +49,26 @@ if __name__ == "__main__":
     path = r"Jaffa/Glz/Glz_resized.jpeg"
     img = cv2.imread(path)
 
-    # run clustering
-    h1, h2, vertical_p = find_vanishing_points(img,
-                          plot_detected=False,
-                          iter=1000,
-                          segmentetion_algorithm="LSD"
-                          )
-
-    draw_line_on_img(img, h1, h2, color=(255, 0, 0), show=True)
+    # # run clustering
+    # h1, h2, vertical_p = find_vanishing_points(img,
+    #                       plot_detected=False,
+    #                       iter=1000,
+    #                       segmentetion_algorithm="LSD"
+    #                       )
 
     # b = (190, 400)
     # r = (189, 278)
     # b0 = (445, 584)
+    # H = 166
     # t0 = (451, 462)
-
-    h1 = (int(1253.615038), int(318.433753))
-    h2 = (int(-474.315634), int(293.168654))
+    name = path.split('/')[-1].split(".")[0]
+    input_path = f"input_files/{name}/{name}.txt"
+    h1, h2, vertical = extract_points_from_text(input_path)
     b, r = choose_coordinates(image=img, message="Please choose the bottom and top points of your reference object.")
     print(f"the top point is {r} and the bottom point is {b}")
     b0, t0 = choose_coordinates(image=img, message="Please choose the bottom and top points of the object you "
                                                    "wold like to measure.")
     H = int(input("Please enter the height of your reference object"))
-    print(calculate_H(b, r, H, b0, t0, h1, h2))
+    print(f"The measured height of your object using cross is: {calculate_H(b, r, H, b0, t0, h1, h2)}")
+    print(f"The measured height of your object using camera matrix is: "
+          f"{calculate_height_using_camera_matrix(b, r, H, b0, t0, h1, h2, vertical)}")
